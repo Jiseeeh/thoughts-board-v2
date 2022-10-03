f4484fb51979064057b74c8cf281c1d1f4e31fea
jiseeeh <johncarlo.camara1@gmail.com>
2022-10-03 14:17:59 +0800

    feat: add navbar and mui css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ScopedCssBaseline } from "@mui/material";
+import type { AppProps } from "next/app";
+import "../styles/globals.css"
+import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ScopedCssBaseline>
+        <Navbar />
+        <Component {...pageProps} />
+      </ScopedCssBaseline>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
