5cf68e94c7288e4a37f851463c05408cb0b446b3
jiseeeh <johncarlo.camara1@gmail.com>
2022-10-03 16:11:56 +0800

    feat: add icon & go to home on click

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,9 +5,17 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
+  const router = useRouter();
+
+  function backToHome() {
+    router.push("/");
+  }
+
+  // sx={{bgcolor: '#f9f8fc'}}
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="fixed">
@@ -16,13 +24,22 @@ export default function Navbar() {
             size="large"
             edge="start"
             color="inherit"
-            aria-label="menu"
+            aria-label="icon"
             sx={{ mr: 2 }}
+            style={{
+              height: "3rem",
+              width: "3rem",
+            }}
           >
-            <MenuIcon />
+            <Image
+              src="/home.gif"
+              alt="Home icon"
+              layout="fill"
+              onClick={backToHome}
+            />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Home
           </Typography>
           <Button color="inherit">Sign-in</Button>
           <Button color="inherit">Login</Button>
