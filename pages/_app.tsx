21ae1449f6795b57230fc5e5cf6e8080a13de2f5
Jiseeeh <johncarlo.camara1@gmail.com>
2022-10-05 17:24:20 +0800

    feat: provide UserContext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,27 @@
 import { ScopedCssBaseline } from "@mui/material";
 import type { AppProps } from "next/app";
-import "../styles/globals.css"
+import { useState, useMemo } from "react";
+
+import "../styles/globals.css";
 import Navbar from "../components/Navbar";
+import UserContext from "../lib/UserContext";
+import User from "../interfaces/IUser";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [user, setUser] = useState<User>({
+    username: "",
+    password: "",
+  });
+
+  const userValue = useMemo(() => ({ ...user, setUser }), [user, setUser]);
+
   return (
-    <>
+    <UserContext.Provider value={userValue}>
       <ScopedCssBaseline>
         <Navbar />
         <Component {...pageProps} />
       </ScopedCssBaseline>
-    </>
+    </UserContext.Provider>
   );
 }
 
