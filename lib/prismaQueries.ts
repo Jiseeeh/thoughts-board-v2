de39f3b2a2dd7cbe4b1c7232c81f3954a9c6a5b4
Jiseeeh <johncarlo.camara1@gmail.com>
2022-10-06 21:19:19 +0800

    feat: add login query

--- lib/prismaQueries.ts
+++ lib/prismaQueries.ts
@@ -30,3 +30,21 @@ export async function addUser(username: string, password: string) {
     };
   }
 }
+
+export async function loginUser(username: string, password: string) {
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        username,
+        password,
+      },
+    });
+
+    return user;
+  } catch (error) {
+    return {
+      success: false,
+      error,
+    };
+  }
+}
