a98b044da0f4a7df9407f6ef2238c3b638e8c06f
Jiseeeh <johncarlo.camara1@gmail.com>
2022-10-07 00:32:16 +0800

    feat: add redirect to create

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import type { NextPage } from "next";
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
+import { useRouter } from "next/router";
 import FilterListIcon from "@mui/icons-material/FilterList";
 
 import Container from "../components/Container";
 import Button from "../components/Button";
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  const onSubmitAThoughtClick = () => {
+    router.push("/thoughts/create");
+  };
+
   return (
     <>
       <Container>
@@ -18,7 +25,11 @@ const Home: NextPage = () => {
             flexGrow: 1,
           }}
         >
-          <Button submit={false} content="Submit a Thought" />
+          <Button
+            submit={false}
+            content="Submit a Thought"
+            onClick={onSubmitAThoughtClick}
+          />
           <FilterListIcon
             sx={{
               ml: 1,
