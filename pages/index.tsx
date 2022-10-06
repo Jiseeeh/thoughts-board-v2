2cf962109c13c2251372316f5baea04372a4ef7c
Jiseeeh <johncarlo.camara1@gmail.com>
2022-10-06 22:38:44 +0800

    feat: add submit thought & filter button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,30 @@
 import type { NextPage } from "next";
+import { Box, Typography } from "@mui/material";
+import FilterListIcon from "@mui/icons-material/FilterList";
 
 import Container from "../components/Container";
-
+import Button from "../components/Button";
 const Home: NextPage = () => {
   return (
     <>
       <Container>
-        <h1>HELLO</h1>
+        <Box
+          sx={{
+            width: 1,
+            height: 50,
+            display: "flex",
+            justifyContent: "flex-end",
+            alignItems: "center",
+            flexGrow: 1,
+          }}
+        >
+          <Button submit={false} content="Submit a Thought" />
+          <FilterListIcon
+            sx={{
+              ml: 1,
+            }}
+          />
+        </Box>
       </Container>
     </>
   );
