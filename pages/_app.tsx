60aa3dc249432b6e91ef8a9247d1df621b42370c
Jiseeeh <johncarlo.camara1@gmail.com>
2022-10-06 17:08:37 +0800

    feat: check if user is authenticated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,27 @@
 import { ScopedCssBaseline } from "@mui/material";
 import type { AppProps } from "next/app";
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
+import { useRouter } from "next/router";
 
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
 import UserContext from "../lib/UserContext";
 import User from "../interfaces/IUser";
+import { useAuthentication } from "../lib/hooks";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { isAuthenticated, username, password } = useAuthentication();
+  const router = useRouter();
   const [user, setUser] = useState<User>({
     username: "",
     password: "",
   });
 
   const userValue = useMemo(() => ({ ...user, setUser }), [user, setUser]);
+
+  useEffect(() => {
+    if (isAuthenticated) setUser({ username, password });
+  }, [isAuthenticated, password, router, username]);
 
   return (
     <UserContext.Provider value={userValue}>
