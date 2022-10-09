9bb3a6dcaf77bcd56778240bbbe2be6a0375f749
Jiseeeh <johncarlo.camara1@gmail.com>
2022-10-09 14:06:40 +0800

    fix: unable to log in

--- lib/hooks/useAuthentication.ts
+++ lib/hooks/useAuthentication.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
-import { useEffect, useState, useContext } from "react";
-import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 /**
  * It checks if the user is authenticated, if not, it redirects them to the sign in page.
@@ -11,12 +10,10 @@ export default function useAuthentication() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const router = useRouter();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
 
-    if (!token) router.push("/signIn");
     // verify token
     (async function () {
       const response = await axios.post("/api/verify", { token });
@@ -29,7 +26,6 @@ export default function useAuthentication() {
         setIsAuthenticated(true);
         return;
       }
-      router.push("/signIn");
     })();
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
