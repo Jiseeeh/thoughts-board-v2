b91a6de1ced68103347c71643f68149379472817
Jiseeeh <johncarlo.camara1@gmail.com>
2022-10-13 15:20:13 +0800

    feat: create custom mui theme

---
+++ mui/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+
+export default createTheme({
+  typography: {
+    fontFamily: `"Inter", sans-serif`,
+    fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+  },
+});
