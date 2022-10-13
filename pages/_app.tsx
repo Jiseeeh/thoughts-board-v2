1786d428b26c107e8dc8b9f488b5ceba32d11aa6
Jiseeeh <johncarlo.camara1@gmail.com>
2022-10-13 14:29:10 +0800

    feat: remove setting of user on mount

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { ScopedCssBaseline } from "@mui/material";
 import type { AppProps } from "next/app";
-import { useState, useMemo, useEffect } from "react";
+import { useState, useMemo } from "react";
 import { useRouter } from "next/router";
 import { Toaster } from "react-hot-toast";
 
@@ -11,18 +11,12 @@ import User from "../interfaces/IUser";
 import useAuthentication from "../lib/hooks/useAuthentication";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { isAuthenticated, username, password } = useAuthentication();
-  const router = useRouter();
   const [user, setUser] = useState<User>({
     username: "",
     password: "",
   });
 
   const userValue = useMemo(() => ({ ...user, setUser }), [user, setUser]);
-
-  useEffect(() => {
-    if (isAuthenticated) setUser({ username, password });
-  }, [isAuthenticated, password, router, username]);
 
   return (
     <UserContext.Provider value={userValue}>
